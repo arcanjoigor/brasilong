b679dff56aaa01f8392a069e55ec16841c0f0530
igor arcanjo <igor.arc.dev@gmail.com>
2023-04-21 13:45:15 -0300

    implementa media query

--- src/pages/Doar.js
+++ src/pages/Doar.js
@@ -1,13 +1,13 @@
 function Doar () {
     return (
-        <main class="lg:h-screen">
+        <main>
         <section>
             <article class="p-5 xl:px-20 text-center text-blue-800 text-lg m-3">
                 <h1 class="text-4xl text-center mb-4">Faça uma doação agora mesmo:</h1>  
 
                 <div class="mb-6 bg-yellow-50 w-80 mx-auto"><img src="pix.png" class="p-2" alt="pix" /><p>CHAVE PIX</p><p>CNPJ 99.999.999/9999-99</p><p>BANCO XYZ</p></div>
 
-                <div class="mb-6 bg-yellow-50 w-80 mx-auto"><img src="boleto.png" class="p-2" alt="pix" /><p class="p-2">CLICK NO SIMBOLO DO BOLETO PARA GERAR UM BOLETO NO VALOR DE R$50,00</p></div>
+                <div class="mb-6 bg-yellow-50 w-80 mx-auto"><img src="boleto.png" class="p-2" alt="boleto" /><p class="p-2">CLICK NO SIMBOLO DO BOLETO PARA GERAR UM BOLETO NO VALOR DE R$50,00</p></div>
                
                 <div class="text-justify">
                   <p class="mb-6">Em nome da equipe da ONG Brasil Acolhedor, gostaríamos de expressar nossa mais sincera gratidão pela sua doação generosa. Sua ajuda nos permitirá continuar com nossas missões de fornecer acesso à educação para jovens carentes, fornecer alimentação para famílias carentes, fornecer cuidados médicos para idosos carentes, fornecer apoio psicológico para pessoas em situação de vulnerabilidade, fornecer moradia para pessoas em situação de rua e fornecer acesso à cultura e esportes para jovens carentes.</p>
